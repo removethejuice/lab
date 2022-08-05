6d572df8d03f8c861c5be247af7f83b0d3f9d0b1
removethejuice <diegojesuschavezbotto@gmail.com>
2022-08-05 15:15:56 -0600

    leto

---
+++ Lab3P2_DiegoChavez_122211369/src/lab3p2_diegochavez_122211369/Lab3P2_DiegoChavez_122211369.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3p2_diegochavez_122211369;
+
+/**
+ *
+ * @author 50497
+ */
+public class Lab3P2_DiegoChavez_122211369 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
